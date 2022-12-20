6a58a2a7dfa096fd9db6390645a1149a04a18b17
chahk03 <chku52@gmail.com>
2022-12-21 03:22:07 +0900

    init

---
+++ ssalgorithm/src/main/java/com/ssafy/ssalgorithm/config/PropertyConfig.java
@@ -0,0 +1,10 @@
+package com.ssafy.ssalgorithm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:properties/env.properties")
+public class PropertyConfig {
+
+}
---
+++ ssalgorithm/src/test/java/com/ssafy/ssalgorithm/SsalgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssalgorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsalgorithmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
